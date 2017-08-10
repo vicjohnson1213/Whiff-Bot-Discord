344368b9ade7ffa49f940b8de71bbc7211b78751
Victor Johnson <victor@victorjohnson.io>
2017-08-10 00:34:19 -0500

    Fixed whitespace and groups not working.

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ client.on('message', message => {
     if (!message.content.startsWith('!')) {
         return;
     }
-    
+
     let parts = message.content.split(/\s+/);
     let commandName = parts[0].substr(1);
-    
+
     if (commands[commandName]) {
         let command = commands[commandName];
         let args = parts.slice(1);
-        
+
         command.run(message, args);
     } else {
         message.channel.send(`!${commandName} is not a valid command. Use \`!commands\` to see a list of all available commands.`);
