46dc83e01a8ab9939af1c44dde4cf9c9ff805e91
Victor Johnson <vicjohnson1213@gmail.com>
2020-08-07 13:36:23 -0500

    Switch env variables to prod version.

--- WhiffBot/Configuration/Configuration.cs
+++ WhiffBot/Configuration/Configuration.cs
@@ -4,7 +4,7 @@ namespace WhiffBot.Configuration
 {
     public class Configuration : IConfiguration
     {
-        public string DiscordClientId => Environment.GetEnvironmentVariable("WHIFF_BOT_TEST_CLIENT_ID");
-        public string ConnectionString => Environment.GetEnvironmentVariable("WHIFF_BOT_TEST_CONNECTION_STRING");
+        public string DiscordClientId => Environment.GetEnvironmentVariable("WHIFF_BOT_CLIENT_ID");
+        public string ConnectionString => Environment.GetEnvironmentVariable("WHIFF_BOT_CONNECTION_STRING");
     }
 }
